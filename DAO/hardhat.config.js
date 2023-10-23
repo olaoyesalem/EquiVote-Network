fa10ddfb2340b0c69c3dceea26a9027a244247a4
olaoyesalemgreat <olaoyesalemgreat@gmail.com>
2023-10-23 14:23:01 +0000

    updated hardhat.config

--- DAO/hardhat.config.js
+++ DAO/hardhat.config.js
@@ -1,4 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: {
+    version: "0.8.20",
+    settings:{
+      optimizer:{
+        enabled: true,
+        runs:100
+      }
+    }
+  }
 };
