95ebef22c707443ec8157a41592be3b8de2fdfe3
olaoyesalemgreat <olaoyesalemgreat@gmail.com>
2023-10-23 14:22:44 +0000

    Updated Box Contract

--- DAO/contracts/Box.sol
+++ DAO/contracts/Box.sol
@@ -4,17 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
+/** @title   Box
+    @author olaoyesalem
+    @notice  This Box contract is the target contract that we vote on.
+*/
+
+
 contract Box is Ownable{
+
+constructor(address _address ) Ownable( _address){
+
+}
+
 uint256 private value;
 
 event ValueChanged (uint256 newValue);
 
 
-function store(uint256 newValue) public onlyOwner{
+function storeValue(uint256 newValue) public onlyOwner{
     value = newValue;
 
     emit ValueChanged(newValue);
 
+}
+
+function retrieveValue () public view returns(uint256) {
+    return value;
 }
 
 }
